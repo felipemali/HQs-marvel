afc177f103a9d25bd62fb6d6a05038d0b4af7606
Felipe Lima <felipedesouzamali@gmail.com>
2025-08-06 20:03:03 -0400

    criando-tipagem-characters

---
+++ src/models/Characters.ts
@@ -0,0 +1,39 @@
+export interface MarvelAPIResponse {
+  code: number;
+  status: string;
+  data: MarvelData;
+}
+
+export interface MarvelData {
+  offset: number;
+  limit: number;
+  total: number;
+  count: number;
+  results: MarvelCharacter[];
+}
+
+export interface MarvelCharacter {
+  id: number;
+  name: string;
+  description: string;
+  modified: string;
+  thumbnail: MarvelThumbnail;
+  resourceURI: string;
+  comics: MarvelComics;
+}
+
+export interface MarvelThumbnail {
+  path: string;
+  extension: string;
+}
+
+export interface MarvelComics {
+  available: number;
+  returned: number;
+  items: MarvelComicSummary[];
+}
+
+export interface MarvelComicSummary {
+  resourceURI: string;
+  name: string;
+}
