f6d5f8e01582b069ec2e782c510f9a7c1eb6f5bd
Felipe Lima <felipedesouzamali@gmail.com>
2025-08-07 13:48:52 -0400

    novo-local-characters

---
+++ src/pages/Home/components/Character/styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import paperImage from "../../../../assets/img/paper.jpg";
+
+export const CharactersContainer = styled.div`
+  max-width: 80vw;
+  margin: 2em auto 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 0.138fr));
+  grid-gap: 15px;
+  padding: 1em 0 3em 3em;
+  border-radius: 0.5em;
+  background-color: white;
+  background-image: url(${paperImage});
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+  @media (min-width: 551px) and (max-width: 900px) {
+    max-width: 55vw;
+  }
+  @media (min-width: 900px) and (max-width: 975px) {
+    max-width: 58vw;
+  }
+`;
+
+export const CharacterCardContainer = styled.div`
+  border: 6px solid gold;
+`;
+
+export const CharacterCard = styled.div`
+  background: rgba(255, 255, 255, 0.67);
+  padding: 2em;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  border: 2px solid black;
+  box-shadow: 4px 4px black;
+  filter: grayscale(100%);
+  transition: 0.2s linear;
+  position: relative;
+
+  &:hover {
+    cursor: pointer;
+    filter: grayscale(0%);
+    box-shadow: 6px 6px black;
+    transform: translate(-2px, -2px);
+  }
+  @media (min-width: 551px) and (max-width: 900px) {
+    max-width: 70%;
+  }
+  @media (max-width: 550px) {
+    max-width: 60%;
+  }
+`;
