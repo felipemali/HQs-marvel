5b155a55b444cdf959db63b96578ec7fccac5f0f
Felipe Lima <felipedesouzamali@gmail.com>
2025-08-07 11:06:34 -0400

    criando-pagina-details

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { RouteObject } from "react-router-dom";
 import Home from "./pages/Home";
+import Details from "./pages/Details";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/detalhes/:name",
+    element: <Details />,
   },
 ];
 
