f0059deb59d5843c6714f18a57142b9f65f9ffa5
Felipe Lima <felipedesouzamali@gmail.com>
2025-08-10 14:17:33 -0400

    criando-hook-total

---
+++ src/hooks/totalCart.ts
@@ -0,0 +1,11 @@
+import { MarvelComicRarity } from "../models/comicks";
+
+export const useTotalCart = (comics: MarvelComicRarity[]) => {
+  return comics.reduce((acc, comic) => {
+    const comicTotal = comic.prices.reduce(
+      (sum, priceObj) => sum + priceObj.price,
+      0
+    );
+    return acc + comicTotal;
+  }, 0);
+};
