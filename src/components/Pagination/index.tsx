0ba0cbd31940f5bcf278ea8f9e04128a19c80978
Felipe Lima <felipedesouzamali@gmail.com>
2025-08-10 14:16:40 -0400

    refatorando-pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,54 +1,35 @@
-import { useEffect, useState } from "react";
 import { PaginationContainer, PaginationControls, PageButton } from "./style";
 import Comic from "../../pages/Home/components/Comic";
 import { ComicContainer } from "../../pages/Home/components/Comic/styles";
 import { useAppSelector } from "../../hooks";
-import { MarvelComic, MarvelComicsAPIResponse } from "../../models/comicks";
+import {
+  MarvelComicRarity,
+  MarvelComicsAPIResponse,
+} from "../../models/comicks";
+import { useDispatch } from "react-redux";
+import { setCurrentPage } from "../../redux/store/marvelSlice";
 
 type PaginationProps = {
-  comics: MarvelComicsAPIResponse | null;
+  comics: MarvelComicsAPIResponse<MarvelComicRarity> | null;
 };
 export const Pagination = ({ comics }: PaginationProps) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [filtered, setFiltered] = useState<MarvelComic[]>([]);
-  const search = useAppSelector((state) => state.marvel.search);
+  const currentPage = useAppSelector((state) => state.marvel.currentPage);
+
+  const dispatch = useDispatch();
 
   const itemsPerPage = 14;
-
-  useEffect(() => {
-    if (comics) {
-      const filteredResults = comics.data.results.filter((comic) => {
-        if (search) {
-          return comic.title.toLowerCase().includes(search.toLowerCase());
-        }
-        return true;
-      });
-
-      setFiltered(filteredResults);
-    }
-  }, [search]);
-
-  if (!comics || !comics.data || !comics.data.results) {
-    return null;
-  }
-
-  const totalPages = Math.ceil(comics?.data?.results?.length / itemsPerPage);
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const currentItems = comics.data.results.slice(
-    startIndex,
-    startIndex + itemsPerPage
-  );
+  const totalPages = Math.ceil((comics?.data.total ?? 0) / itemsPerPage);
 
   const handlePageChange = (page: number) => {
-    setCurrentPage(page);
+    dispatch(setCurrentPage(page));
   };
 
   return (
     <PaginationContainer>
       <ComicContainer>
-        {search
-          ? filtered.map((item) => <Comic key={item.id} comic={item} />)
-          : currentItems.map((item) => <Comic key={item.id} comic={item} />)}
+        {comics?.data.results.map((item) => (
+          <Comic key={item.id} comic={item} />
+        ))}
       </ComicContainer>
 
       <PaginationControls>
