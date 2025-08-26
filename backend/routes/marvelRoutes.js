60f139cb1d57e1868e001351a0b990527080d7cd
Felipe Lima <felipedesouzamali@gmail.com>
2025-08-25 22:13:18 -0400

    criando rota ping

--- backend/routes/marvelRoutes.js
+++ backend/routes/marvelRoutes.js
@@ -4,5 +4,8 @@ const getComics = require("../controllers/marvelController");
 const router = express.Router();
 
 router.get("/comics", getComics);
+router.get("/ping", (req, res) => {
+  res.status(200).send("ping");
+});
 
 module.exports = router;
