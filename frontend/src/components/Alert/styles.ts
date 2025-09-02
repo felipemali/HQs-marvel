8687911ffa160f353c6c5ea6fce5fcbc26361dcb
Felipe Lima <felipedesouzamali@gmail.com>
2025-09-02 19:02:48 -0400

    mudando o componente Alert

--- frontend/src/components/Alert/styles.ts
+++ frontend/src/components/Alert/styles.ts
@@ -3,13 +3,15 @@ import styled, { css } from "styled-components";
 type AlertType = "success" | "error" | "warning";
 
 export const AlertWrapper = styled.p<{ type: AlertType }>`
+  max-width: 30vw;
+
   font-family: "Bangers", cursive;
   padding: 10px 20px;
   border-radius: 6px;
   color: red;
   font-size: 1.2em;
   color: white;
-  margin: 10px 0;
+  margin: 1em auto 0 auto;
   background-color: white;
   transition: opacity 0.2s ease-in-out;
 
